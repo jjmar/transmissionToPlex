ead6e62ae4e2c8101902fbd5803e0c2f90f2b500
Justin Martin <jjmardev@gmail.com>
2021-01-27 20:16:45 -0500

    Initial commit

---
+++ transmission-to-plex/classifier.py
@@ -0,0 +1,8 @@
+"""
+Determines whether a given folder contains a movie or a series (tv show)
+by looking at the number of video files a the folder has
+"""
+
+
+def classify_downloaded_torrent(list_of_files):
+    pass
